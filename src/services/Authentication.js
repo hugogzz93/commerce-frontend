68cff4454ad18697d07b2f50ce71520b3b638122
Hugo González <pinelo93@gmail.com>
2019-01-25 13:47:32 -0600

    Fixes cookie read/write

--- src/services/Authentication.js
+++ src/services/Authentication.js
@@ -1,6 +1,7 @@
 import cookie from 'cookie'
 import { sendMutation, sendQuery } from '../lib/api'
 import { LOG_IN_MUTATION, LOG_OUT_MUTATION, LOG_IN_JWT_QUERY } from '../constants/schema'
+import docCookies from '../lib/docCookies'
 
 export const login = ({email, password}) => sendMutation({
     mutation: LOG_IN_MUTATION,
@@ -21,10 +22,7 @@ export const setAuthTokenCookie = token => {
   let today = new Date()
   let expires = new Date()
   expires.setTime(today.getTime() + 3600000 * 24 * 14) //14 days from now
-  document.cookie = `auth_token=${token};expires=${expires.toGMTString()}`
+  docCookies.setItem('auth_token', token, expires.toGMTString(), '/', 'localhost');
 }
 
-export const getAuthToken = () => {
-  const raw_token = document.cookie.split(';').filter(item => item.includes('auth_token='))
-  return raw_token.length ? raw_token[0].split('=')[1] : null
-} 
+export const getAuthToken = () => ( docCookies.getItem('auth_token'))
