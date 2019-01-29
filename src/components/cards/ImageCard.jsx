849feb253ca1d3cc26af1d3866d33fe445a04627
Hugo González <pinelo93@gmail.com>
2019-01-29 15:25:20 -0600

    Fixes image url

--- src/components/cards/ImageCard.jsx
+++ src/components/cards/ImageCard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import '../../style/cards/imagecard.sass'
 
 const ImageTag = ({source, alt}) => {
+  if(source) source = source.replace(new RegExp(' ','g'), '%20')
   return(
     <div 
       className="image-card"
