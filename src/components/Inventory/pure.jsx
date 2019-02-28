0ce39b30d8b4b6ad67ea508cc719af5e6328b4a7
Hugo González <pinelo93@gmail.com>
2019-02-28 16:41:08 -0600

    Fixes inventory debounced updates

--- src/components/Inventory/pure.jsx
+++ src/components/Inventory/pure.jsx
@@ -6,12 +6,20 @@ import '../../style/inventory.sass'
 const Inventory = props => {
   const [ userProducts, setUserProducts ] = useState([])
   const [ filter, setFilter ] = useState('')
+  const [ debouncedRegistry, setDebounced] = useState({})
 
   useEffect(() => {
     if(!props.user_id) return
     props.getUserProducts({id: props.user_id})
           .then( items => setUserProducts(items))
   }, [props.user_id])
+
+  const registerDebounced = (id, fn) => {
+    if(debouncedRegistry[id]) return debouncedRegistry[id]
+    const debouncedFn = debounce(fn, 300)
+    setDebounced({...debouncedRegistry, [id]: debouncedFn})
+    return debouncedFn
+  }
 
   const updateItemStock = ({id, stock}) => {
     const updatedItem = {...userProducts.find(e => e.id == id), stock}
@@ -21,15 +29,16 @@ const Inventory = props => {
   const userProductDivs = userProducts
   .filter(e => e.name.toLowerCase().includes(filter.toLowerCase()))
   .map(uProd => {
+    const updateItems = registerDebounced(uProd.id, (value, originalValue, id) => 
+          props.updateStock({stock: parseInt(value), id})
+                .catch(() => updateItemStock({id, stock: originalValue}))
+    ) 
+
     const onInputChange = e => {
       const originalValue = uProd.stock
       const newValue = e.target.value
       updateItemStock({id: uProd.id, stock: newValue})
-
-      debounce(() =>
-        props.updateStock({stock: parseInt(newValue), id: uProd.id})
-            .catch(() => updateItemStock({id: uProd.id, stock: originalValue}))
-      , 300 )()
+      updateItems(newValue, originalValue, uProd.id)
     }
     return (
       <div key={uProd.id} className="inv__item fade-in">
